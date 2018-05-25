6f77dc2c00a13d241868de57ec87af6859abe3d3
Danielthweatt <dthweatt192@yahoo.com>
2018-05-24 20:22:34 -0500

    started working on controller

---
+++ controllers/controller.js
@@ -0,0 +1,12 @@
+// Modules
+const express = require("express");
+const db = require("../models");
+
+// Router Setup
+const router = express.Router();
+
+// Router
+
+
+// Export
+module.exports = router;
