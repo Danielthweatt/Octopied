31ca065bae0666f3436c2b051b118959f44c1109
Abraham <abeis@msn.com>
2018-05-28 23:31:12 -0500

    added level up and exp functions

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,6 +2,7 @@
 //Seting up Main variables
 let count = 0;
 let clickCount = 0;
+const expGrothModifier = 50;
 let resources ={
     hearts: 0,
     babbies:0,
@@ -10,6 +11,23 @@ let resources ={
     rock:0,
     steel:0
 }
+
+let octoStats = {
+    level: 1,
+    exp: 0,
+    prestidge: 0,
+    proficiency :{
+        food:1,
+        attack:1,
+    },
+    abilities: {
+        foodFrenzy :0,
+        inkSpray :0,
+        rankUp: 0
+    }
+}
+
+
 
 
 $(document).on('click','.octo',clickFrenzy);
@@ -26,12 +44,16 @@ function enable() {
  * Keeps track of the numbers of Clicks 
  */
 function clickFrenzy() {
-   clickCount++
+    const clickValue = calcualteClickValue();
+   clickCount += clickValue;
+   gainExperiance();
+   levelup();
    $('.counter').text(clickCount);
-   if(clickCount === 10){
+   if(octoStats.level === 10){
        alert('Oh Something Happening');
       evolve();
    }
+
 }
 
 /**
@@ -56,4 +78,31 @@ function evolve() {
 },180);
 }
 
+
+function calcualteClickValue(){
+     const proficiency = (octoStats.proficiency.food * .05) + 1;
+     const tool =  0//toolA + toolB + toolC;
+     const baseCollect = 1;
+     const collectValue = baseCollect + tool;
+     const clickValue = Math.ceil( ((octoStats.level * collectValue) * proficiency ));
+
+     return clickValue;
+}
+
+function gainExperiance() {
+    //exp items still need to be made
+    const expItem = 1.25;
+    const gainExperiance = octoStats.prestidge * (expItem) +1;
+    octoStats.exp += gainExperiance;
+    $('.current-exp').text(`Exp: ${octoStats.exp}`)
+}
+
+function levelup(){
+    console.log(octoStats.exp, octoStats.level)
+    if(octoStats.exp > octoStats.level * expGrothModifier){
+       alert('level up')
+        octoStats.level ++;
+        $('.currnet-level').text(`Level:${octoStats.level}`)
+    }
+}
 
