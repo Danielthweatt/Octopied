886a3270e5bca482d80d0ae99e49084e9a818fc0
landon <landon.seebart@gmail.com>
2018-06-05 20:33:36 -0500

    moved validation to auth.js updated layout

--- public/javascript/auth.js
+++ public/javascript/auth.js
@@ -1,9 +1,22 @@
 $(function(){
-
-    $("#reset").validate({
+    $("#signin").validate({
         rules: {
-            repass: {
-            equalTo: "#pass"
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+                minlength: 8
+              }
+        },
+        messages: {
+            email:{
+                required: "Email is required.",
+            },
+            password: {
+                required: "Password is required.",
+                minlength: "Password must be at least 8 characters."
             }
         },
         errorElement : 'div',
@@ -19,8 +32,25 @@ $(function(){
 
     $("#signup").validate({
         rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+                minlength: 5
+              },
             repass: {
-            equalTo: "#pass"
+                equalTo: "#pass"
+            }
+        },
+        messages: {
+            email:{
+                required: "Email is required.",
+            },
+            password: {
+                required: "Password is required.",
+                minlength: "Password must be at least 5 characters."
             }
         },
         errorElement : 'div',
@@ -34,4 +64,40 @@ $(function(){
         }
     });
 
+    $("#reset").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+                minlength: 5
+              },
+            repass: {
+                equalTo: "#pass"
+            }
+        },
+        messages: {
+            email:{
+                required: "Email is required.",
+            },
+            password: {
+                required: "Password is required.",
+                minlength: "Password must be at least 5 characters."
+            }
+            repass: {
+                equalTo: "Passwords must match."
+            }
+        },
+        errorElement : 'div',
+        errorPlacement: function(error, element) {
+            var placement = $(element).data('error');
+            if (placement) {
+                $(placement).append(error)
+            } else {
+                error.insertAfter(element);
+            }
+        }
+    });
 });
